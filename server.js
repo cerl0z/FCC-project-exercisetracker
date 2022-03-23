ffc607edeb5d8b393d0b40f776a2be5c302dcb9c
Carlos Peña <cpena3@u.rochester.edu>
2022-03-23 14:10:59 -0400

    submit to github

--- server.js
+++ server.js
@@ -1,18 +1,150 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const Schema = mongoose.Schema;
+const { v4: uuidv4 } = require("uuid");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
+const userSchema = new Schema({
+  _id: String,
+  username: String,
+  count: Number,
+  log: [
+    {
+      description: String,
+      duration: Number,
+      date: Date,
+    },
+  ],
 });
 
+const exerciseSchema = new Schema({
+  description: String,
+  duration: Number,
+  date: Date,
+});
 
+const User = mongoose.model("User", userSchema);
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+// const Log = mongoose.model("Log", logSchema);
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.post("/api/users", async (req, res) => {
+  const userName = req.body.username;
+  const userId = uuidv4();
+  let findUser = await User.findOne({ username: userName });
+
+  //if user exists return user
+  if (findUser) {
+    res.json({
+      _id: findUser._id,
+      username: findUser.username,
+    });
+  }
+  // if user does not exist save new user and return user
+  else {
+    findUser = new User({
+      _id: userId,
+      username: userName,
+    });
+    await findUser.save();
+
+    res.json({
+      username: findUser.username,
+      _id: findUser._id,
+    });
+  }
+});
+
+app.get("/api/users", async (req, res) => {
+  let allUsers = await User.find({}).select("username _id"); //find all
+
+  res.json(allUsers);
+});
+
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  const userId = req.params._id;
+  const descriptionToAdd = req.body.description;
+  const durationToAdd = req.body.duration;
+  const dateToAdd = req.body.date;
+
+  const exObject = {
+    description: descriptionToAdd,
+    duration: durationToAdd,
+    date: dateToAdd,
+  };
+  if (dateToAdd === "") {
+    dateToAdd = new Date().toString();
+  }
+  let user = await User.findByIdAndUpdate({ _id: userId });
+  let newExercise = await Exercise(exObject);
+
+  if (user) {
+    user.log.push(exObject);
+  }
+  await user.save();
+  await newExercise.save();
+
+  res.json({
+    username: user.username,
+    description: newExercise.description,
+    duration: newExercise.duration,
+    date: newExercise.date,
+    _id: user._id,
+  });
+});
+
+app.get("/api/users/:_id/logs/:from?/:to?/:limit?", async (req, res) => {
+  let userId = req.params._id;
+  let from = req.params.from;
+  let to = req.params.to;
+  let limit = req.params.limit;
+  let user = await User.findById({ _id: userId });
+
+  let count = user.log.length;
+  //console.log(`from:${from}, to:${to}, limit:${limit}`);
+  if (user) {
+    if (from && to) {
+      let filterLog = user.log;
+      //console.log(`from:${from}, to:${to}, limit:${limit}`);
+      filterLog.filter(
+        (log) => log.date.valueOf() >= from && log.date.valueOf() <= to
+      );
+      if (limit) {
+        filterLog = filterLog.slice(0, limit);
+      }
+
+      res.json({
+        log: filterLog,
+      });
+    } else {
+      let exLog = user.log.map((log) => ({
+        description: log.description,
+        duration: log.duration,
+        date: log.date,
+      }));
+
+      console.log("user found");
+      res.json({
+        username: user.username,
+        count: count,
+        _id: userId,
+        log: exLog,
+      });
+    }
+  }
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
